45274c729e44e0a4b21cb6ed981597ad42d53b7f
yaelVerso <kevinjohnbalingit.bersola@gmail.com>
2025-11-13 23:36:28 +0800

    all files added
    v1

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAwLe5yh3-D1K5pnqbkC7ovAJ35dywVgs",
+    authDomain: "mymood-journal-563f5.firebaseapp.com",
+    projectId: "mymood-journal-563f5",
+    storageBucket: "mymood-journal-563f5.firebasestorage.app",
+    messagingSenderId: "541813233736",
+    appId: "1:541813233736:web:2afb68f36772a2c9e8207d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
